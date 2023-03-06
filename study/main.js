0308e9bd13aaf07a97e93ff38e9178ad3b474225
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2023-03-06 14:04:40 +0900

    study

---
+++ study/main.js
@@ -0,0 +1,37 @@
+const http = require('http');
+
+// http.createServer((req,res) => {
+//     res.write('hi goodday');
+//     res.end();
+// }).listen(3000);
+
+// const app = http.createServer(function (req, res) {
+//     res.write('hey?');
+//     res.end();
+// });
+
+// app.listen(3000);
+
+// const app = http.createServer(function (req, res) {
+//     res.writeHead(200, {'Contenet-Type':'text/html; charset=UTF-8'});
+//     res.write('안녕?');
+//     res.end();
+// });
+
+// app.listen(3000);
+
+// const app = http.createServer( (req, res) => {
+//     res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
+//     res.end('하이');
+// });
+
+// app.listen(3000, () => {
+//     console.log('Starting server 3000');
+// });
+
+// http.createServer( (req, res) => {
+//     res.writeHead(200, {'Content-Type':'text/html; charset=UTF-8'});
+//     res.end('.....');
+// }).listen(3000, () => {
+//     console.log('Starting server 3000');
+// });
