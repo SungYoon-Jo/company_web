0a7a61757474108f34c0763ae833ac300d99a4be
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2023-03-20 20:55:04 +0900

    3.3

--- momentum/app.js
+++ momentum/app.js
@@ -1,1 +1,7 @@
- 
+const title = document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick() {
+  console.log("nice~");
+}
+
+title.addEventListener("click", handleTitleClick);
