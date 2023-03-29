c1defd2f7938b25a50a54ffb45afb86386a7d186
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2023-03-29 17:56:37 +0900

    end 4

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,9 +1,18 @@
 export const trending = (req, res) => {
   res.end("trend");
 };
-export const watch = (req, res) => {
-  res.send("watch");
+export const see = (req, res) => {
+  return res.send("watch");
 };
 export const edit = (req, res) => {
-  res.send("edit");
+  return res.send("edit");
+};
+export const search = (req, res) => {
+  res.send("search");
+};
+export const upload = (req, res) => {
+  res.send("upload");
+};
+export const deleteVideo = (req, res) => {
+  return res.send("deleteVideo");
 };
