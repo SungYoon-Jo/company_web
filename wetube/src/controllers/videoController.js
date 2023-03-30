f2498103eebe7ad10c578124809da1997ce845c2
goodday <37609646+SungYoon-Jo@users.noreply.github.com>
2023-03-30 18:55:59 +0900

    6-2

--- wetube/src/controllers/videoController.js
+++ wetube/src/controllers/videoController.js
@@ -1,18 +1,41 @@
+let videos = [
+  {
+    title: "First Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 1,
+    id: 1,
+  },
+  {
+    title: "Second Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 2,
+  },
+  {
+    title: "Third Video",
+    rating: 5,
+    comments: 2,
+    createdAt: "2 minutes ago",
+    views: 59,
+    id: 3,
+  },
+];
+
 export const trending = (req, res) => {
-  res.render("home", { pageTitle: "Home" });
+  return res.render("home", { pageTitle: "Home", videos });
 };
-export const see = (req, res) => {
-  res.render("watch");
+export const watch = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("watch", { pageTitle: `Watching: ${video.title}`, video });
 };
-export const edit = (req, res) => {
-  res.render("edit");
+export const getEdit = (req, res) => {
+  const { id } = req.params;
+  const video = videos[id - 1];
+  return res.render("edit", { pageTitle: `Editing: ${video.title}`, video });
 };
-export const search = (req, res) => {
-  res.send("search");
-};
-export const upload = (req, res) => {
-  res.send("upload");
-};
-export const deleteVideo = (req, res) => {
-  return res.send("deleteVideo");
-};
+export const postEdit = (req, res) => {};
